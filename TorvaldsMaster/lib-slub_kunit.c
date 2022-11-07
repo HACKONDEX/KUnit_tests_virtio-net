9c09532b303a6af4f5029e4f6a90bba2994a89e8
sagateljan akop <sagatelyan.aa@phystech.edu>
2022-11-07 18:40:54 +0300

    lib-slub_kunit.c

---
+++ TorvaldsMaster/lib-slub_kunit.c
@@ -0,0 +1,12 @@
+static struct kunit_case test_cases[] = {
+// 	KUNIT_CASE(test_clobber_zone),
+
+// #ifndef CONFIG_KASAN
+// 	KUNIT_CASE(test_next_pointer),
+// 	KUNIT_CASE(test_first_word),
+// 	KUNIT_CASE(test_clobber_50th_byte),
+// #endif
+
+// 	KUNIT_CASE(test_clobber_redzone_free),
+	{}
+};
--- TorvaldsMaster/readme.md
+++ TorvaldsMaster/readme.md
@@ -6,6 +6,14 @@
 
 - [drivers/net/virtio_test.c](virtio_test.c)
 
+-----------------
+
+- [lib/slub_kunit.c](lib-slub_kunit.c)
+
+- [lib/Kconfig.debug](lib-Kconfig.debug)
+
+-----------------
+
 - [net/core/dev_addr_lists_test.c](net-core-dev_addr_lists_test.c)
 
 - [net/Kconfig](net-Kconfig)
