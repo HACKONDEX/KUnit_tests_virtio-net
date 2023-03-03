c83b11369ffa2c281c75beb928ad8ed8c988a4a3
sagateljan akop <sagatelyan.aa@phystech.edu>
2023-03-03 20:44:34 +0300

    old configs removed

--- TorvaldsMaster/net-core-dev_addr_lists_test.c
+++
@@ -1,9 +0,0 @@
-static struct kunit_case dev_addr_test_cases[] = {
-	// KUNIT_CASE(dev_addr_test_basic),
-	// KUNIT_CASE(dev_addr_test_sync_one),
-	// KUNIT_CASE(dev_addr_test_add_del),
-	// KUNIT_CASE(dev_addr_test_del_main),
-	// KUNIT_CASE(dev_addr_test_add_set),
-	// KUNIT_CASE(dev_addr_test_add_excl),
-	{}
-};
